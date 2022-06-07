1703c08d9051f5bad6ac3b57d3d86b0d19124d6a
profkosmosic <61395703+profkosmosic@users.noreply.github.com>
2022-06-07 09:47:48 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeroTabComponent } from './hero-tab/hero-tab.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "heroes/:key", component: HeroTabComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
